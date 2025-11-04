17d0d03da404e35b26837ba12b82776132c3f562
Clara <claracruz.alves908@gmail.com>
2025-11-04 14:21:44 -0300

    arrumando alguns arquivos 3

--- src/main/java/com/example/demo/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/demo/exceptions/ResourceExceptionHandler.java
@@ -1,27 +1,22 @@
 package com.example.demo.exceptions;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.HashMap;
 import java.util.Map;
 
 //@ControllerAdvice
-public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
+public class ResourceExceptionHandler{
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, WebRequest request) {
-
         HttpStatus status = HttpStatus.NOT_FOUND; // 404
-
         StandardError err = new StandardError(
                 System.currentTimeMillis(),
                 status.value(),
@@ -33,13 +28,8 @@ public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(err, status);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            HttpHeaders headers,
-            HttpStatusCode status,
-            WebRequest request) {
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<Object> handleValidationException(MethodArgumentNotValidException ex, WebRequest request) {
         Map<String, String> validationErrors = new HashMap<>();
         ex.getBindingResult().getAllErrors().forEach((error) -> {
             String fieldName = ((FieldError) error).getField();
